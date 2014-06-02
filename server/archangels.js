b28948c75c0c3b99a47e0dab21b03712ad508b06
ted.choi <tedhaha@gmail.com>
2014-06-02 10:30:09 +0000

    update

--- server/archangels.js
+++ server/archangels.js
@@ -30,7 +30,7 @@ if ('development' == env) {
 // connect to database
 // =============================================================================
 
-var _DBPool = require('./lib/_DBPool');
+var pool = require('./lib/_DBPool');
 
 // =============================================================================
 // router setting
@@ -40,15 +40,15 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.json({ message: 'welcome to our Archangels' });   
 });
-router.get('/users', function(req,res){
-    _DBPool.acquire(function(err, db) {
+
+// dbtest retrieve users
+router.get('/dbtest/users', function(req,res){
+    pool.acquire(function(err, conn) {
         if (err) {
             return res.end("CONNECTION error: " + err);
         }
-
-        db.query("select * from user",[],function(err, rows, columns) {
-        _DBPool.release(db);
- 
+        conn.query("select * from user",[],function(err, rows, columns) {
+        pool.release(conn);
         if (err) {
           return res.end("QUERY ERROR: " + err);
         }
@@ -57,6 +57,153 @@ router.get('/users', function(req,res){
   });
 });
 
+// retrieve player game status
+router.get('/api/users/pgs/:id', function(req,res){
+    var user_id =  req.params.id;
+    var gain_gold = req.params.gain_gold;
+console.log(gain_gold);
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.query('SELECT * from player_game_status where pgs_user_id= ? ',user_id,function (err, rows, columns) {
+        console.log(rows[0].pgs_gold);
+console.log(rows);
+
+        pool.release(conn);
+        if (err) {
+          return res.end("QUERY ERROR: " + err);
+        }
+        res.end(JSON.stringify(rows));
+    });
+  });
+});
+
+// retrieve player game status
+router.put('/api/users/pgs/:id', function(req,res){
+    var user_id =  req.params.id;
+    var gold_gain =  req.params.id;
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.query('SELECT * from player_game_status where pgs_user_id= ? ',user_id,function (err, rows, columns) {
+        pool.release(conn);
+        if (err) {
+          return res.end("QUERY ERROR: " + err);
+        }
+        res.end(JSON.stringify(rows));
+    });
+  });
+});
+
+// retrieve player game status
+router.get('/api/users/pgs', function(req,res){
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.query("select * from player_game_status",[],function(err, rows, columns) {
+        pool.release(conn);
+        if (err) {
+          return res.end("QUERY ERROR: " + err);
+        }
+        res.end(JSON.stringify(rows));
+    });
+  });
+});
+
+// retrieve users
+router.get('/api/users', function(req,res){
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.query("select * from user",[],function(err, rows, columns) {
+        pool.release(conn);
+        if (err) {
+          return res.end("QUERY ERROR: " + err);
+        }
+        res.end(JSON.stringify(rows));
+    });
+  });
+});
+
+
+
+// retrieve user
+router.get('/api/users/:id', function(req,res){
+    var user_id =  req.params.id;
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.query('SELECT * from user where user_id= ? ',user_id,function (err, rows, columns) {
+        pool.release(conn);
+        if (err) {
+          return res.end("QUERY ERROR: " + err);
+        }
+        res.end(JSON.stringify(rows));
+    });
+  });
+});
+
+// create user
+router.post('/api/users', function(req,res){
+    //TODO: email 중복체크
+    var user = {
+                  'user_pid':req.body.user_pid
+                  ,'user_email':req.body.user_email
+                  ,'user_password':req.body.user_password
+                  ,'user_nickname':req.body.user_nickname
+            };
+ 
+    pool.acquire(function(err, conn) {
+        if (err) {
+            return res.end("CONNECTION error: " + err);
+        }
+        conn.beginTransaction(function(err) {
+            if (err) {
+                throw err;
+            }
+            //conn.query('INSERT INTO user VALUES ?', user, function (err, result) {
+            conn.query('INSERT INTO user (user_pid,user_email,user_password,user_nickname) VALUES (?,?,?,?)',
+                    [req.body.user_pid,req.body.user_email,req.body.user_password,req.body.user_nickname], function (err, result) {
+                if (err) {
+                    console.error(err);
+                    conn.rollback(function () {
+                        console.error('rollback error');
+                        throw err;
+                    });
+                }// if err
+                console.log('insert transaction log');
+                user_id = result.insertId;
+                console.log('result: ',result);
+
+                conn.query('INSERT INTO player_game_status (pgs_user_id) values (?)',user_id, function (err, result) {
+                     if (err) {
+                         console.error(err);
+                         conn.rollback(function () {
+                            console.error('rollback error');
+                             throw err;
+                          });
+                      }// if err
+                     conn.commit(function (err) {
+                        if (err) {
+                            console.error(err);
+                            conn.rollback(function () {
+                                   console.error('rollback error');
+                                   throw err;
+                                });
+                        }// if err
+                        res.send(200, 'success');
+     
+                     });// commit
+                    });// insert into log
+            });// inset into users
+        }); // begin trnsaction
+    });
+});
 
 
 
